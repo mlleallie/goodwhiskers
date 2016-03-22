0956c3824c374d33484ff01e9c33b353b0f3d463
Allie Sudholt <allison.sudholt@gmail.com>
2016-03-22 09:41:22 -0500

    added category seed data

--- goodwhiskers/db/seeds.rb
+++ goodwhiskers/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: "Oil")
+Category.create(name: "Balm")
+Category.create(name: "Clippers")
+Category.create(name: "Wax")
+Category.create(name: "Cream")
+Category.create(name: "Gel")
+Category.create(name: "Brush")
+Category.create(name: "Softener")
