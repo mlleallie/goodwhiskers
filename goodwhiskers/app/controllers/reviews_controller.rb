fee8b1e45b0e001ed90d2e53f3c7ce0812fe40f3
Allie Sudholt <allison.sudholt@gmail.com>
2016-03-21 20:08:29 -0500

    added new review and review to products_show

--- goodwhiskers/app/controllers/reviews_controller.rb
+++ goodwhiskers/app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+    @product = Product.find(params[:product_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @product = Product.find(params[:product_id])
+    @review.product = @product
+    @review.save
+    redirect_to product_path(@product)
+
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:comment, :rating, :user_id, :product_id)
+  end
+
 end
