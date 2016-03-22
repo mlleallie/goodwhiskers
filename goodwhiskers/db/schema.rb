8179e5967da570232c26ab13cbc92be06c9f416b
Allie Sudholt <allison.sudholt@gmail.com>
2016-03-21 20:25:42 -0500

    added title column to reviews

--- goodwhiskers/db/schema.rb
+++ goodwhiskers/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321192416) do
+ActiveRecord::Schema.define(version: 20160322011946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160321192416) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "title"
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
