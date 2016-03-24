5499821e79d75f00e7664e897cdc0f3813d7d0db
Allie <allison.sudholt@gmail.com>
2016-03-23 19:09:46 -0500

    Update schema.rb

--- goodwhiskers/db/schema.rb
+++ goodwhiskers/db/schema.rb
@@ -106,7 +106,6 @@ ActiveRecord::Schema.define(version: 20160323214437) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.text     "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
